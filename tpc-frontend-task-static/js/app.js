1efe28783e7472fa8996fb4327bbf9391a27c166
Jose Martorell <josemartorell@MacBook-Pro-de-Jose.local>
2017-03-25 16:40:53 +0100

    Task07
    Added questions array (test)

---
+++ tpc-frontend-task-static/js/app.js
@@ -0,0 +1,7 @@
+var questions = [
+	new Question["I find rabbits confusing",["Very confusing","Not confusing","Prefer not to say","What?"], "Not confusing"],
+	new Question["I find rabbits funny",["Very funny","No","Prefer not to say","What?"], "No"],
+	new Question["I find rabbits lovely",["Very lovely","Not confusing","Prefer not to say","What?"], "Not confusing"],
+	new Question["I find rabbits hateful",["Yes","Not hateful","Prefer not to say","What?"], "Yes"],
+	new Question["I find rabbits charming",["Very charming","Always","Prefer not to say","What?"], "Always"],
+];
