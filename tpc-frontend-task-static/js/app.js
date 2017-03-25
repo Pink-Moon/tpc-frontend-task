3729e2ff0d9b8b87c30683a6b35eabfd3b0bada7
Jose Martorell <josemartorell@MacBook-Pro-de-Jose.local>
2017-03-25 17:56:48 +0100

    Revert "Task08"
    This reverts commit a8e46310667b1bbb29be4c9e23e787afedb0d890.

--- tpc-frontend-task-static/js/app.js
+++ tpc-frontend-task-static/js/app.js
@@ -1,23 +1,7 @@
-function populate() {
-	if(quiz.isEnded()) {
-		
-
-	}
-	else {
-		
-		var element = document.getElementById("question");
-		element.innerHTML = quiz.getQuestionIndex().text;
-	}
-}
-
 var questions = [
-	new Question("I find rabbits confusing", ["Very confusing","Not confusing","Prefer not to say","What?"], "Not confusing"),
-	new Question("I find rabbits funny", ["Very funny","No","Prefer not to say","What?"], "No"),
-	new Question("I find rabbits lovely", ["Very lovely","Not confusing","Prefer not to say","What?"], "Not confusing"),
-	new Question("I find rabbits hateful", ["Yes","Not hateful","Prefer not to say","What?"], "Yes"),
-	new Question("I find rabbits charming", ["Very charming","Always","Prefer not to say","What?"], "Always"),
+	new Question["I find rabbits confusing",["Very confusing","Not confusing","Prefer not to say","What?"], "Not confusing"],
+	new Question["I find rabbits funny",["Very funny","No","Prefer not to say","What?"], "No"],
+	new Question["I find rabbits lovely",["Very lovely","Not confusing","Prefer not to say","What?"], "Not confusing"],
+	new Question["I find rabbits hateful",["Yes","Not hateful","Prefer not to say","What?"], "Yes"],
+	new Question["I find rabbits charming",["Very charming","Always","Prefer not to say","What?"], "Always"],
 ];
-
-var quiz = new Quiz(questions);
-
-populate();
